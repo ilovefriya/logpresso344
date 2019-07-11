afc07e2ffd7c9eb1d9dbd12ee662b28877fe62ba
ilovefriya <ilovefriya@gmail.com>
2019-07-11 21:04:56 +0900

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -1,5 +1,3 @@
-docker build --tag logpresso:3.4.4 .
+docker build --tag ilovefriya/logpresso:3.4.4 .
 
-docker run -d --name logpresso logpresso:3.4.4 -p 8888:8888 -p 7022:7022  -v "$(pwd)/data:/home/logpresso/data"
-
-docker run -d --name logpresso logpresso:3.4.4 -p 8888:8888 -p 7022:7022  -v "$(pwd)/data:/home/logpresso/data" /bin/bash -c "/home/logpresso/server/logpresso.sh & sleep infinity"
+docker run -d -p 8888:8888 -p 7022:7022 --name logpresso logpresso:3.4.4 /bin/bash -c "/home/logpresso/server/logpresso.sh & sleep infinity" --volume $(pwd)/data:/home/logpresso/data 
